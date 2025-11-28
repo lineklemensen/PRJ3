59b174e51f7b7d9a6650086cdcc10e3661631cf6
Dani211e <daniel.jenrich@gmail.com>
2025-11-28 08:37:29 +0100

    Comment json

--- src/tui/src/HTTP/HttpHandler.cpp
+++ src/tui/src/HTTP/HttpHandler.cpp
@@ -4,11 +4,12 @@
 
 void HttpHandler::send_route(Route r, std::promise<cpr::Response>&& res)
 {
+    // "visit room 1 and room 3
+    //E.g {"rooms": [1,0,1]}
     const std::string json = "{" + std::format("\"rooms\":[{},{},{}]", r.rooms[0], r.rooms[1], r.rooms[2]) + "}";
 
     //Fake network delay
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
+    //std::this_thread::sleep_for(std::chrono::seconds(10));
 
     res.set_value(cpr::Post(cpr::Url{"http://" + URL + ":" + PORT + ROUTE},
                             cpr::Body{json},
