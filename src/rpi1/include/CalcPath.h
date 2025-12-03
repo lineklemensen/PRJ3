6ff65da365f3d8863df7e2c4b3eb5394830f3273
Christopher F. Jørgensen <202300791@post.au.dk>
2025-12-03 10:22:14 +0100

    Added the code for calculating turning degree and driving length

---
+++ src/rpi1/include/CalcPath.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#ifndef CALC_PATH_H
+
+#define CALC_PATH_H
+
+class DR{
+    public:
+    std::pair <double, double> current_pos = {0, 0};
+    std::pair <double, double> last_pos = {current_pos.first, current_pos.second};
+    
+
+    DR(){};
+    std::pair <double, double> calc_route(std::pair <double, double> next_pos);
+
+    
+ 
+
+
+
+};
+
+
+#endif
